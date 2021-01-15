1ed7de21b333967fb2e27a068f8f429d51153733
xreaad <ryadhchalbi0@gmail.com>
2021-01-14 20:21:52 -0600

    assets files :apple:

---
+++ assets/js/App.js
@@ -0,0 +1,12 @@
+// query selector
+
+const select = (element) => {
+    return document.querySelector(element);
+}
+
+let menuToggler = select('.menu-toggle');
+let body = select('body');
+
+menuToggler.addEventListener('click', () => {
+    body.classList.toggle('open')
+})
